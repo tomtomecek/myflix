5c1c87f62d3f1a3603ffa83a36b6c496b9a3abc2
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-21 16:17:55 +0100

    add categories controller, index, routes and adjust home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  get 'home', to: "videos#index"
+  get 'home', to: "categories#index"
+  resources :categories, only: [:show]
   resources :videos, only: [:show]
 end
