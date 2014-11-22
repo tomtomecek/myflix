791d8d3a1e573e9f9fe072353f84111efa17a9e2
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-22 16:51:45 +0100

    First video spec

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe Video do
+  it "saves a new Video" do
+    vid = Video.new(title: "Terminator", 
+                    description: "Skynet takes over the world", 
+                    small_cover_url: "/tmp/terminator.jpg", 
+                    large_cover_url: "/tmp/terminator_large.jpg")
+    vid.save
+    
+    expect(Video.first.title).to eq("Terminator")
+    expect(Video.first.description).to eq("Skynet takes over the world")
+    expect(Video.first.small_cover_url).to eq("/tmp/terminator.jpg")
+    expect(Video.first.large_cover_url).to eq("/tmp/terminator_large.jpg")
+  end
 end
