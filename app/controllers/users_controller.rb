0fec80a9af362567e418a6e0efca05665b5cbddf
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-22 21:33:22 +0100

    add register sign in front page routes, users controler

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to myFlix, you have successfully registered."
+      redirect_to home_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :fullname)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
+  root "static_pages#front"
+
+  get '/sign_in', to: "sessions#new"
+  post '/sign_in', to: "sessions#create"
+  get '/register', to: "users#new"
+  post '/register', to: "users#create"
+
   get 'home', to: "categories#index"
+
   resources :categories, only: [:show]
   resources :videos, only: [:show] do
     collection do
