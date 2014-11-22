8464af2d4eaa301eda2402f9e797c0db626a61cb
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-22 17:01:57 +0100

    Category specs

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Category do
+  
+  it "saves itself" do
+    cat = Category.new(name: "TV shows")
+    cat.save
+    expect(Category.first).to eq(cat)
+  end
+
+  it "has many videos" do
+    cat = Category.create(name: "Movies")
+    video1 = Video.create(title: "Terminator1", category: cat)
+    video2 = Video.create(title: "Terminator2", category: cat)
+    expect(Category.first.videos).to include(video1, video2)
+  end
+
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,10 +7,17 @@ describe Video do
                     small_cover_url: "/tmp/terminator.jpg", 
                     large_cover_url: "/tmp/terminator_large.jpg")
     vid.save
-    
+
     expect(Video.first.title).to eq("Terminator")
     expect(Video.first.description).to eq("Skynet takes over the world")
     expect(Video.first.small_cover_url).to eq("/tmp/terminator.jpg")
     expect(Video.first.large_cover_url).to eq("/tmp/terminator_large.jpg")
+  end
+
+
+  it "can have a category" do
+    cat = Category.create(name: "Movies")
+    vid = Video.create(title: "Terminator", category: cat)
+    expect(Video.first.category).to eq(Category.first)
   end
 end
