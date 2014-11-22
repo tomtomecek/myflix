bd628aaaa3b5ebb2671eec9b1ea0cda7a9f878d6
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-22 19:07:55 +0100

    Add self.search_by_title method via TDD

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates :title, :description, presence: true
+
+  def self.search_by_title(title)
+    where("title LIKE ?", "%#{title}%")
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,24 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+  describe "#self.search_by_title" do
+
+    it "should fail fiding if no video" do
+      Video.create(title: "Interstellar", description: "a big step for humans")
+      expect(Video.search_by_title("Inception")).to_not eq(Video.first)
+    end
+
+    it "should find 1 video by title" do
+      Video.create(title: "Interstellar", description: "a big step for humans")
+      expect(Video.search_by_title("Interstellar")).to eq([Video.first])
+    end
+
+    it "should find multiple videos by title matching pattern" do
+      Video.create(title: "Interstellar", description: "a big step for humans")
+      Video.create(title: "stellarium", description: "will be released in 2016")
+      expect(Video.search_by_title("stella")).to eq([Video.first, Video.second])
+    end
+  end
+
+
 end
