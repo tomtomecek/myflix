1a2d4127687ca368f9c40e80a239335b22c5d3bb
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-22 20:46:37 +0100

    add recent_video to category spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order("title") }
+  has_many :videos, -> { order(:title) }
+
+  def recent_videos
+    self.videos.reorder(created_at: :desc).limit(6)
+  end
 end
