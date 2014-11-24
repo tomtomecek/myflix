9fec22bbb39d84f5bbf519c62f595326ed0e4af7
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-24 21:16:16 +0100

    refactor category spec even more

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,24 +6,25 @@ describe Category do
 
   describe ".recent_videos" do
     let(:category) { Fabricate(:category) }
+    subject { category.recent_videos }
     
     it "returns max 6 videos" do
       Fabricate.times(7, :video, category: category)
-      expect(category.recent_videos.count).to be <= 6
+      subject.count.should be <= 6
     end
 
     it "returns all videos if less than 6" do
       Fabricate.times(3, :video, category: category)
-      expect(category.recent_videos.count).to be <= 6
+      subject.count.should be <= 6
     end
 
     it "returns chronological reversed order" do
       vid1 = Fabricate(:video, category: category, created_at: 2.days.ago)
       vid2 = Fabricate(:video, category: category, created_at: 1.day.ago)
       vid3 = Fabricate(:video, category: category)
-      expect(category.recent_videos).to eq([vid3, vid2, vid1])
+      should eq([vid3, vid2, vid1])
     end
 
-    it { expect(category.recent_videos).to eq([]) }
+    it { should eq([]) }
   end
 end
