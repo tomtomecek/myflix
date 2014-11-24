931680f37eed2904d67f8c0593af746c3a09e5bf
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-24 21:07:04 +0100

    refactor category specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,35 +2,28 @@ require 'spec_helper'
 
 describe Category do
  
-  it { should have_many(:videos) }
+  it { should have_many(:videos).order(:title) }
 
   describe ".recent_videos" do
+    let(:category) { Fabricate(:category) }
     
-    it "should return max 6 videos" do
-      cat = Category.create(name: "TV shows")
-      8.times { Video.create(title: "title", description: "desc", category: cat) }
-      expect(cat.recent_videos.count).to be <= 6
+    it "returns max 6 videos" do
+      Fabricate.times(7, :video, category: category)
+      expect(category.recent_videos.count).to be <= 6
     end
 
-    it "should return all videos if less than 6" do
-      cat = Category.create(name: "TV shows")
-      3.times { Video.create(title: "title", description: "desc", category: cat) }
-      expect(cat.recent_videos.count).to be <= 6
+    it "returns all videos if less than 6" do
+      Fabricate.times(3, :video, category: category)
+      expect(category.recent_videos.count).to be <= 6
     end
 
-    it "should return chronological reversed order" do
-      cat = Category.create(name: "TV shows")
-      vid1 = Video.create(title: "title1", description: "first", category: cat)
-      vid2 = Video.create(title: "title2", description: "second", category: cat)
-      vid3 = Video.create(title: "title3", description: "third", category: cat)
-      expect(cat.recent_videos).to eq([vid3, vid2, vid1])
+    it "returns chronological reversed order" do
+      vid1 = Fabricate(:video, category: category, created_at: 2.days.ago)
+      vid2 = Fabricate(:video, category: category, created_at: 1.day.ago)
+      vid3 = Fabricate(:video, category: category)
+      expect(category.recent_videos).to eq([vid3, vid2, vid1])
     end
 
-    it "returns an empty array if no video" do
-      cat = Category.create(name: "TV shows")
-      expect(cat.recent_videos).to eq([])
-    end
-
+    it { expect(category.recent_videos).to eq([]) }
   end
-
 end
