26230c53adeb283be397061bb24a304cc7849d9b
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-25 01:02:10 +0100

    add authorization spec to reviews controller spec

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  #before_action :require_user
+  before_action :require_user
 
   def create
     @video = Video.find(params[:video_id])
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,32 +1,42 @@
 require 'spec_helper'
 
 describe ReviewsController do
-  let(:user) { Fabricate(:user) }
-  let(:video) { Fabricate(:video) }
-  #before { session[:user_id] = user.id }
-
+  
   describe "POST create" do
+    let(:user) { Fabricate(:user) }
+    let(:video) { Fabricate(:video) }
     
-    context "creates the review if valid" do
-      before do
-        post :create, video_id: video.id, review: Fabricate.attributes_for(:review, user: user, video: video)
+    context "signed in" do
+      before { session[:user_id] = user.id }
+
+      context "review record valid" do
+        before do
+          post :create, 
+            video_id: video.id, 
+            review: Fabricate.attributes_for(:review, user: user, video: video)
+        end
+
+        it { expect(assigns(:video)).to eq(video) }
+        it { expect(Review.count).to    eq(1) }      
+        it { expect(response).to        redirect_to video }
       end
 
-      it "creates the review under video if record is valid" do        
-        expect(assigns(:video)).to eq(video)
-        expect(Review.count).to eq(1)
-      end      
-      it { expect(response).to redirect_to video }
-    end
-
-    context "does not create the review if invalid" do
-      before do 
-        post :create, video_id: video.id, review: Fabricate.attributes_for(:review, body: "")
+      it "review record invalid" do
+        post :create, 
+          video_id: video.id, 
+          review: Fabricate.attributes_for(:review, body: "")
+        expect(response).to render_template 'videos/show'
       end
-
-      it { expect(response).to render_template 'videos/show' }
     end
-        
+
+    context "not signed in" do
+      it "redirects to the root url" do
+        post :create, 
+          video_id: video.id, 
+          review: Fabricate.attributes_for(:review, user: user, video: video)
+        expect(response).to redirect_to root_url
+      end
+    end
   end
   
 end
