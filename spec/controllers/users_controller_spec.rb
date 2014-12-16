8a61cad071352fcc9b8350937e697a973f668064
Frenzy277 <tomas.tomecek@hotmail.com>
2014-12-16 01:34:21 +0100

    add user mailer specs for welcome_email

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,14 +10,36 @@ describe UsersController do
   end
 
   describe "POST create" do
-    context "valid input" do
-      before { post :create, user: Fabricate.attributes_for(:user) }
-    
+    context "valid input" do    
       it "creates the user" do
+        post :create, user: Fabricate.attributes_for(:user)
         expect(User.count).to eq(1)
       end
       
-      it { should redirect_to sign_in_url }
+      it "redirects to sign_in_url" do
+        post :create, user: Fabricate.attributes_for(:user)
+        is_expected.to redirect_to sign_in_url
+      end
+
+      context "email sending" do
+        it "sends out the email" do
+          post :create, user: Fabricate.attributes_for(:user)
+          expect(ActionMailer::Base.deliveries).not_to be_empty
+        end
+        
+        it "sends to the right recipient" do
+          post :create, user: Fabricate.attributes_for(:user, email: "alice@example.com")
+          message = ActionMailer::Base.deliveries.last
+          expect(message.to).to eq(["alice@example.com"])
+        end
+
+        it "has the right content" do
+          post :create, user: Fabricate.attributes_for(:user, email: "alice@example.com")
+          message = ActionMailer::Base.deliveries.last
+          require 'pry'; binding.pry
+          expect(message.body.encoded).to include("Welcome")
+        end
+      end
     end
 
     context "invalid input" do
