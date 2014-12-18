fa6d57630f1739755d2504d958a5e7bc67168fa4
Frenzy277 <tomas.tomecek@hotmail.com>
2014-12-18 11:07:04 +0100

    expiring token on successfull invitation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       UserMailer.welcome_email(@user).deliver
-      if params[:invitation_token]
-        invitation = Invitation.find_by(token: params[:invitation_token])
-        if invitation
-          Relationship.create(leader: invitation.sender,follower: @user)
-          Relationship.create(leader: @user, follower: invitation.sender)
-        end  
-      end
+      handle_invitations(params[:invitation_token]) if params[:invitation_token]
       flash[:success] = "Welcome to myFlix, you have successfully registered."
       redirect_to sign_in_url
     else
@@ -27,10 +21,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  private
+private
 
-    def user_params
-      params.require(:user).permit(:email, :password, :fullname)
+  def user_params
+    params.require(:user).permit(:email, :password, :fullname)
+  end
+
+  def handle_invitations(token)
+    invitation = Invitation.find_by(token: token)
+    if invitation
+      Relationship.create(leader: invitation.sender,follower: @user)
+      Relationship.create(leader: @user, follower: invitation.sender)
+      invitation.update_column(:token, nil)
     end
-
+  end
 end
