a2e442869ea9db2b7e293493962760909d2c1ca0
tomas-tomecek <tomas.tomecek@hotmail.com>
2014-12-19 20:40:45 +0100

    adding background jobs to all emailing

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,7 +9,7 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new(invitation_params)
 
     if @invitation.save
-      InvitationMailer.send_invite_to_a_friend(@invitation).deliver
+      InvitationMailer.delay.send_invite_to_a_friend(@invitation)
       flash[:success] = "You have invited your friend #{@invitation.recipient_name} to MyFLiX"
       redirect_to home_url
     else
