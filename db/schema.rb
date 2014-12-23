b9150a18d217b18d53fb1bb55f718c905e40ea33
tomtomecek <tomas.tomecek@hotmail.com>
2014-12-23 11:23:32 +0100

    add video_url column to videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223092831) do
+ActiveRecord::Schema.define(version: 20141223102248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20141223092831) do
     t.integer  "category_id"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
   add_index "videos", ["category_id"], name: "index_videos_on_category_id", using: :btree
