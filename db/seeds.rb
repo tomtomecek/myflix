1838e11c2fadd1413f13764ddcd8a62ef660862b
tomtomecek <tomas.tomecek@hotmail.com>
2014-12-23 21:56:40 +0100

    update seeds for carrierwave upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,17 @@ Fabricate.times(4, :user)
 tv_shows = Category.create(name: "TV shows")
 movies = Category.create(name: "Movies")
 
-["Interstellar", "V for Vendetta", "Inception", "District 9", 
-  "Family guy", "Monk", "South Park", "Futurama"].each_with_index do |title, index|
-  video = Fabricate(:video,
-    category: (index < 4 ? movies : tv_shows),
-    title: title,
-    small_cover: "/tmp/#{title.gsub(' ', '_').downcase}.jpg",
-    large_cover: "/tmp/#{title.gsub(' ', '_').downcase}_large.jpg"
+["Interstellar", "V for Vendetta", "Inception", "District 9", "Family guy", "Monk", 
+  "South Park", "Futurama"].each_with_index do |title, index|
+  video = Fabricate.build(:video, 
+    title: title, 
+    category: (index < 4 ? movies : tv_shows), 
     video_url: "https://s3.amazonaws.com/tt-myflix/Interstellar+Movie+-+Official+Trailer+3.mp4")
+  video.small_cover.store!(File.open(File.join(Rails.root, "public/tmp/#{title.gsub(' ', '_').downcase}.jpg")))
+  video.large_cover.store!(File.open(File.join(Rails.root, "public/tmp/#{title.gsub(' ', '_').downcase}_large.jpg")))
+  video.save!
 
-  15.times { Fabricate(:review, video: video, user: User.all.sample) }
+  5.times { Fabricate(:review, video: video, user: User.all.sample) }
 end
 
 4.times { Fabricate(:relationship, follower: tom) }
