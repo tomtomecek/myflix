8f8dba3bfea5994da11e3ab46349063c5932f591
tomtomecek <tomas.tomecek@hotmail.com>
2014-12-24 17:14:50 +0100

    fixing bug causing raven work in development

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
-require 'raven'
+if Rails.env.staging? || Rails.env.production?
+  require 'raven'
 
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+  end
 end
