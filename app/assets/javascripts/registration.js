01accc66c3866d4df053ffe219b40bf4c5fa0184
tomtomecek <tomas.tomecek@hotmail.com>
2014-12-27 11:47:03 +0100

    rework of registration.js file to be fully compatible with strip.js v2

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,25 +1,21 @@
 jQuery(function($) {
   $('#payment-form').submit(function(event) {
     var $form = $(this);
-    $form.find('.payment-submit').prop('disabled', true);
-    Stripe.card.createToken({
-      number: $('.card-number').val(),
-      cvc: $('.card-cvc').val(),
-      exp_month: $('.card-expiry-month').val(),
-      exp_year: $('.card-expiry-year').val()
-    }, stripeResponseHandler);
+    $form.find('button').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
     return false;
   });
 
-  var stripeResponseHandler(status, response) {
+  function stripeResponseHandler(status, response) {
     var $form = $('#payment-form');
+
     if (response.error) {
       $form.find('.payment-errors').text(response.error.message);
-      $form.find('.payment-submit').prop('disabled', false);
+      $form.find('button').prop('disabled', false);
     } else {
       var token = response.id;
       $form.append($('<input type="hidden" name="stripeToken" />').val(token));
       $form.get(0).submit();
     }
-  }
+  };
 });
