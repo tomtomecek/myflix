0b8721b8620f0c36dc3db6f9e7d4c5ed4d2b1492
tomtomecek <tomas.tomecek@hotmail.com>
2014-12-31 01:13:14 +0100

    finish on video rating decorator

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -1,11 +1,7 @@
 class VideoDecorator < Draper::Decorator
   delegate_all
 
-  def dec_rating
-    if model.rating
-      "#{model.rating} / 5.0"
-    else
-      "N/A"
-    end
+  def rating
+    model.rating.present? ? "#{model.rating} / 5.0" : "N/A"
   end
 end
