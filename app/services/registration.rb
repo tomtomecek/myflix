26e5dec536155de547d0d90822a7392bb50742c4
tomtomecek <tomas.tomecek@hotmail.com>
2015-01-03 00:27:25 +0100

    first attempt to create service object

--- app/services/registration.rb
+++ app/services/registration.rb
@@ -1,7 +1,45 @@
 class Registration
 
-  def initialize(user)
+  attr_accessor :charge_error
+  attr_reader :user, :stripe_token, :invitation_token
+  def initialize(user, options={})
     @user = user
+    @stripe_token = options[:stripe_token]
+    @invitation_token = options[:invitation]
   end
-  
+
+  def successfull?
+    if user.valid?
+      charge = StripeWrapper::Charge.create(
+        amount: 999,
+        card: stripe_token,
+        description: "sign up payment for #{user.email}"
+      )
+      if charge.successfull? && user.save
+        UserMailer.delay.welcome_email(user.id)
+        handle_invitation if invitation_token
+        true
+      else
+        self.charge_error = charge.error_message
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def charging_error_message
+    charge_error
+  end
+
+private
+
+  def handle_invitation
+    invitation = Invitation.find_by(token: invitation_token)
+    if invitation
+      user.follow(invitation.sender)
+      invitation.sender.follow(user)
+      invitation.update_column(:token, nil)
+    end
+  end
 end
--- spec/services/registration_spec.rb
+++ spec/services/registration_spec.rb
@@ -1,5 +1,13 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe Registration do
-  
-end
+# describe Registration do
+#   describe ".new" do
+#     it "initializes registration with user" do
+#       alice = Fabricate(:user)
+#       Registration.new(alice)
+#       expect(assigns(:user)).to eq(alice)
+#     end
+#     it "initializes registration with stripe token"
+#     it "initializes registration with invitation"
+#   end
+# end
