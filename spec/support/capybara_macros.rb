0ad8391f966a9010b79b36da105b6d9f4a2d8a10
Tomas Tomecek <tomas.tomecek@hotmail.com>
2018-03-15 18:35:36 +0100

    Refactor sign in macro with memoization

--- spec/support/capybara_macros.rb
+++ spec/support/capybara_macros.rb
@@ -1,5 +1,5 @@
-def sign_in(a_user=nil)
-  user = a_user || Fabricate(:user)
+def sign_in(user=nil)
+  user ||= Fabricate(:user)
   visit sign_in_path
   fill_in "email",    with: user.email
   fill_in "password", with: user.password
