46be67316846b7d91ec7e5dbab6b8abe2b3c79d2
Tomas Tomecek <tomas.tomecek@hotmail.com>
2018-03-15 14:55:53 +0100

    Set default url host for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,4 +15,5 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :stderr
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: "localhost:52662" }
 end
