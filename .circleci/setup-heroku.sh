96f896ea1b4e3da99d3fc3c83c7251bb1bb29bd2
Tomas Tomecek <tomas.tomecek@hotmail.com>
2018-03-16 22:37:31 +0100

    Rename the ENV variable

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -5,8 +5,8 @@ wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 cat > .netrc <<EOF
 machine api.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_TOKEN
+  password $HEROKU_API_KEY
 machine git.heroku.com
   login $HEROKU_EMAIL
-  password $HEROKU_API_TOKEN
+  password $HEROKU_API_KEY
 EOF
